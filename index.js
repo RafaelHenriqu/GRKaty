c17324047fadbcf7640a9f5ac12afc36b8779f92
Henrique <123227224+RafaelHenriqu@users.noreply.github.com>
2024-07-04 22:15:49 -0300

    Update 0.1.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Mathematics //
+const os = require('os')
 
 function Pi(){
     return Math.PI
@@ -40,18 +40,26 @@ function Percentage(Number1,Number2){
     return (Number1 / 100) * Number2
 }
 
-function Binary(text,Reverse){
+function Binary(text,reverse){ 
 const ASCII_TABLE = require("./ASCII Table.json")
-    if (Reverse){ // Trasnforming Code Binary in text,in Comming soon
-        return 'System In Comming Soon'
-    }else{
-let Text2 = " "
+if (reverse){ 
+    let Text2 = ""
+    let tc = text.split(' ')
+    for (i=0;text.split(' ').length > i;i++){
+        if (tc[i]=='')return Text2
+        Text2 = Text2 + ASCII_TABLE[tc[i]] 
+        Text2 = Text2 + ""
+    }
+    return Text2
+}else{
+    let Text2 = ""
     for (i=0;text.length > i;i++){
         Text2 = Text2 + ASCII_TABLE[text[i]]
         Text2 = Text2 + " "
     }
-    return Text2    
+    return Text2
 }
+
 }
 
 function RandomNumber(Max){
@@ -62,16 +70,51 @@ function Division(Number1,Number2){
     return Number1 / Number2
 }
 
+// Config of Destkop //
+
+function CPU(){
+    return os.cpus()[0].model
+}
+
+function Memory(text_formatted){
+    const Converte = (os.totalmem() / 1073741824).toString()
+    if (text_formatted){
+        const Memory = `${Converte.split('.')[0]},${Converte.split('.')[1][0]} GB`
+        return Memory
+    }else{ 
+        const Memory = Converte.split('.')[0]
+        return Memory
+    }
+    
+    
+}
+
+function Desktop_Name(){
+    return os.hostname()  
+}
+
+function System(){
+    return os.platform()
+}
+
+function UserName(){
+    return os.userInfo()["username"]
+}
 
 module.exports={
     Pi,
-    Add,
-    Division,
     Factorial,
     Fibonacci,
+    Add,
     Multiply,
     Subtraction,
+    Percentage,
     Binary,
     RandomNumber,
-    Division
+    Division,
+    CPU,
+    Memory,
+    Desktop_Name,
+    System,
+    UserName,
 }
